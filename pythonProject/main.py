9c5cd1e239128501c387047820e3a4a34c466085
pavlotamashchuk <pavlo.tamashchuk@gmail.com>
2023-11-09 20:36:21 +0200

    First Commit

---
+++ pythonProject/main.py
@@ -0,0 +1,17 @@
+import math
+
+a = float(input("число 1 "))
+b = int(input("число 2 "))
+c = int(input("число 3 "))
+
+print()
+print("сума  " + str(a + b + c))
+print("різниця  " + str(a - b - c))
+print("добуток  " + str(a * b * c))
+print("частка  " + str(a / b / c))
+
+print("степінь  " + str(a ** b ** c))
+print("поділити націло  " + str(a // b // c))
+print("знайти отсачу  " + str(a % b % c ))
+print("корень  " + str(math.sqrt(a + b + c)))
+#калькулятор Пашы
